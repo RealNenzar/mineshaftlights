26dc881629a9e798f4973795f2575d13594cab72
Nenzar <63953799+RealNenzar@users.noreply.github.com>
2025-06-20 23:31:04 +0200

    Add Files

---
+++ mineshaftlights/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Enhanced Lighting and Open Entrance for the Mineshaft'
+author 'Dean - Cringe Gaming https://discord.gg/DMaTEaf5ta'
+version '1.0.0'
+
+this_is_a_map 'yes'
